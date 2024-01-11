871dbb42d4ae977c369f15179f92fc7c2444ff32
bayuiswara <iswarabayu95@gmail.com>
2024-01-11 13:05:20 +0700

    Update MyStepdefs.java

--- src/test/java/step/MyStepdefs.java
+++ src/test/java/step/MyStepdefs.java
@@ -4,7 +4,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
-import io.cucumber.java.en.When;
+
 import org.junit.jupiter.api.Assertions;
 import pages.BukalapakPages;
 

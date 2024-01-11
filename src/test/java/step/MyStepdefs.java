95905e0b13b06b24d6e7e3240f888fd61704bc01
bayuiswara <iswarabayu95@gmail.com>
2024-01-11 13:00:17 +0700

    Update MyStepdefs.java

--- src/test/java/step/MyStepdefs.java
+++ src/test/java/step/MyStepdefs.java
@@ -10,10 +10,11 @@ import pages.BukalapakPages;
 
 public class MyStepdefs {
     BukalapakPages bl = new BukalapakPages();
+
     @Given("User successfully open website Bukalapak.com")
     public void userSuccessfullyOpenWebsiteBukalapakCom() throws InterruptedException {
         String dir = System.getProperty("user.dir");
-        System.setProperty("webdriver.chrome.driver", dir  + "/driver/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", dir + "/driver/chromedriver.exe");
         Thread.sleep(1000);
         bl.openBukalapak();
 
@@ -21,42 +22,47 @@ public class MyStepdefs {
 
     @io.cucumber.java.en.When("^User Input 'Bola Basket' in search bar$")
     public void userInputBolaBasketInSearchBar() throws InterruptedException {
-      Thread.sleep(1000);
-      bl.inputText("Bola Basket");
+        Thread.sleep(1000);
+        bl.inputText("Bola Basket");
     }
 
     @And("User Click search")
-    public void userClickSearch() {bl.buttonSearch();
+    public void userClickSearch() {
+        bl.buttonSearch();
     }
 
     @Then("Product {string} displayed")
     public void productBolaBasketDisplayed(String text) {
         System.out.println(bl.verifyresultsearch());
-    Assertions.assertEquals(text, bl.verifyresultsearch());
+        Assertions.assertEquals(text, bl.verifyresultsearch());
     }
 
     @And("User sort by highest price")
-    public void userSortByHighestPrice() {bl.buttonSorting();
+    public void userSortByHighestPrice() {
+        bl.buttonSorting();
     }
 
     @Then("Product Bola Basket sort by {string}")
     public void productBolaBasketSortByTermahal(String text) {
         System.out.println(bl.verifyresultsorting());
-        Assertions.assertEquals(text,bl.verifyresultsorting());
+        Assertions.assertEquals(text, bl.verifyresultsorting());
     }
 
     @Then("Product page {string} displayed")
     public void productPageBolaBasketMoltenBGBGSizeFIBAApprovedDisplayed(String text) {
         System.out.println(bl.verifyproductpage());
-        Assertions.assertEquals(text,bl.verifyproductpage());
+        Assertions.assertEquals(text, bl.verifyproductpage());
     }
 
-   @And("User Scrolling Product")
-    public void userScrollingProduct() {bl.scrollproduct();
+    @And("User Scrolling Product")
+    public void userScrollingProduct() {
+        bl.scrollproduct();
     }
 
 
     @And("User choose DONIC MOBILE BALL BASKET COACHY PENAMPUNG BOLA LATIHAN TENIS MEJA ORIGINAL")
-    public void userChooseDONICMOBILEBALLBASKETCOACHYPENAMPUNGBOLALATIHANTENISMEJAORIGINAL() {bl.chooseproduct();
+    public void userChooseDONICMOBILEBALLBASKETCOACHYPENAMPUNGBOLALATIHANTENISMEJAORIGINAL() {
+        bl.chooseproduct();
     }
+}
 
